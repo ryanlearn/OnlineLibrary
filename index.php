3813096e5b96df674692a4fdc5f5533540463770
Ryan Clements <rclements@innovasium.com>
2015-08-03 15:13:56 -0400

    Style Changed to MyBooks page

--- index.php
+++ index.php
@@ -53,7 +53,8 @@
 
     <!-- UI Libs -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="Scripts/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+    <!-- <script src="Scripts/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
     
 
 
